9d60a72a92eed2edeede02bf2da9a0b0ec44e4c7
Abdulla Hassan <ahassan@milestechnologies.com>
2020-11-02 20:43:56 -0500

    Added Final Score, Rounds, and Error Correction for user input

--- javascript.js
+++ javascript.js
@@ -2,29 +2,60 @@ function getRandomInt(max){
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function rds() {
-  let answers = ["rock", "paper", "scissors"];
-  let HumanAnswer = prompt("Please enter you answer: ").toLowerCase;
-  let MachineAnswer = answers[getRandomInt(3)];
-  console.log("Human answer: " + HumanAnswer);
-  console.log("Machine answer: " + MachineAnswer);
-  console.log("Random number: " + (getRandomInt(3) - 1));
-  console.log(HumanAnswer);
+function playGame() {
+  let numberOfRounds = prompt("How many rounds do you wanna play? ");
+  let HumanScore = 0;
+  let MachineScore = 0;
+
+  //loop over for each round
+  for(let i=0; i < numberOfRounds; i++){
+    let answers = ["rock", "paper", "scissors"];
+    let PromptInput = prompt("Please enter you answer: ");
+    let HumanAnswer = PromptInput.toLowerCase();
+    let MachineAnswer = answers[getRandomInt(3)];
+    console.log(`--------------Round ${i+1}---------------`);
+
+    //Human answer is not one of the options, then automtically make answer "rock"
+    if(HumanAnswer != "rock" || HumanAnswer != "paper" || HumanAnswer != "scissors"){
+      HumanAnswer = "rock";
+    }
 
 
-  if (HumanAnswer.toLowerCase === MachineAnswer) {
-    console.log("It's a tie!");
-  } else if (
-    (HumanAnswer.toLowerCase === "rock" && MachineAnswer === "scissors") ||
-    (HumanAnswer.toLowerCase === "paper" && MachineAnswer === "rock") ||
-    (HumanAnswer.toLowerCase === "scissors" && MachineAnswer === "paper")
-  ) {
-    console.log("You Win!");
-  } else if (
-    (HumanAnswer.toLowerCase === "rock" && MachineAnswer === "paper") ||
-    (HumanAnswer.toLowerCase === "paper" && MachineAnswer === "scissors") ||
-    (HumanAnswer.toLowerCase === "scissors" && MachineAnswer === "rock")
-  ) {
-    console.log("You Lose! (sorry bud)");
+    if (HumanAnswer === MachineAnswer) {
+      console.log(`Human: ${HumanAnswer}, Machine: ${MachineAnswer}`);
+      console.log(`Current Score: Human(${HumanScore}) Machine(${MachineScore})`)
+      console.log(`Result: It's a tie!`);
+    } else if (
+      (HumanAnswer === "rock" && MachineAnswer === "scissors") ||
+      (HumanAnswer === "paper" && MachineAnswer === "rock") ||
+      (HumanAnswer === "scissors" && MachineAnswer === "paper")
+    ) {
+      HumanScore++;
+      console.log(`Human: ${HumanAnswer}, Machine: ${MachineAnswer}`);
+      console.log(`Current Score: Human(${HumanScore}) Machine(${MachineScore})`)
+      console.log("Result: You Win!");
+    } else if (
+      (HumanAnswer === "rock" && MachineAnswer === "paper") ||
+      (HumanAnswer === "paper" && MachineAnswer === "scissors") ||
+      (HumanAnswer === "scissors" && MachineAnswer === "rock")
+    ) {
+      MachineScore++;
+      console.log(`Human: ${HumanAnswer}, Machine: ${MachineAnswer}`);
+      console.log(`Current Score: Human(${HumanScore}) Machine(${MachineScore})`)
+      console.log("Result: You Lose!");
+    }
+  
   }
+
+
+  //final score
+  console.log(`--------------Final Score---------------`);
+  console.log(`Human(${HumanScore}) Machine(${MachineScore})`)
+  if(HumanScore > MachineScore){
+    console.log("You Win!!")}
+  else if(HumanScore === MachineScore){
+    console.log(`It's a tie!`);
+  }
+  else{
+    console.log("You Loose :( (sorry bud)")}
 };
